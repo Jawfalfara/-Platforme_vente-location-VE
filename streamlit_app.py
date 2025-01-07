b11f5abba3e0cc171e7555bbbd7e6b61367c8426
Jawfalfara <Xelkomdata@outlook.com>
2025-01-07 18:50:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,4 @@
-import streamlit as st
-import warnings
-import pandas as pd 
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
