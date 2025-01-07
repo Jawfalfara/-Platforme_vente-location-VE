b6249dfcc08a70b9141c23a0686b613c6b73e894
Jawfalfara <Xelkomdata@outlook.com>
2025-01-07 18:48:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import warnings
-warnings.filterwarnings('ignore')
-# Import necessary libraries
-import pandas as pd # type: ignore
+import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
