fc45c6668bf46716130f93c1f87da4114a2b6b68
Jawfalfara <Xelkomdata@outlook.com>
2025-01-07 21:49:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
 
 import numpy as np
 import pandas as pd
